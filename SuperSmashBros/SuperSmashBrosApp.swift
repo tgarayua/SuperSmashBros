62b2c047516c9a35b2cc13b9c4332140ae758697
Thomas Garayua <tgarayua@github.com>
2023-03-15 15:27:53 -0400

    Initial Commit

---
+++ SuperSmashBros/SuperSmashBrosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperSmashBrosApp.swift
+//  SuperSmashBros
+//
+//  Created by Thomas Garayua on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SuperSmashBrosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
